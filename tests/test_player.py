fe49b2f0c2407e8e6f64b5e64c362189796a6b9c
maryamBa01 <maryam.bahiya0195@stud.hkr.se>
2025-10-22 14:47:23 +0200

    Update test_player.py

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,11 +1,2 @@
 
-# holding each players points and name
-class Player:
 
-    def __init__(self, name):
-        self.high_score = 0 # high score for that specific player, 0 to begin with
-        self.name = name
-
-    # add points to the player
-    def add_points(self, points_to_add):
-        self.high_score += points_to_add
