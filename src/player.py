58f894a3d507c9f5d8e0bf405b52fa57c81a384e
Filip Öhlin <fille8@live.se>
2025-10-24 17:28:01 +0200

    Lint fix missing class/function docstring / C0115, C0116

--- src/player.py
+++ src/player.py
@@ -1,16 +1,17 @@
-# holding each players points and name
 class Player:
+    """Player class"""
     def __init__(self):
-        self.high_score = 0  # player's high score (begins with 0)
-        self.is_computer = self.ask_if_computer()  # player type (computer)
+        """Initial setup for player"""
+        self.high_score = 0  # Player's high score (begins with 0)
+        self.is_computer = self.ask_if_computer()  # Player type (computer)
         if self.is_computer:
             self.name = 'Computer'
         else:
             self.name = self.choose_name()
 
-    # ask if player is computer or person
     def ask_if_computer(self):
-        # use built in "lower" function to convert capital C and P
+        """Ask if player is computer or person"""
+        # Use built in "lower" function to convert capital C and P
         player_type = input(
             'Do you want this player to be a computer or a person? '
             '"c" - computer, or "p" - person'
@@ -23,10 +24,10 @@ class Player:
             print('Invalid input. Please try again!')
             return self.ask_if_computer()
 
-    # player chooses what name they want to be displayed as
     def choose_name(self):
+        """Player chooses what name they want to be displayed as"""
         return input('What name do you want to display yourself as?')
 
-    # add points to the player
     def add_points(self, points_to_add):
+        """Add points to the player"""
         self.high_score += points_to_add
